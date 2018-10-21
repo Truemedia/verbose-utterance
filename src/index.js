7c44c250a91f96915dbe9d87ff9c9e1574eafdf0
Wade Penistone <wadespackages@yahoo.com>
2018-10-21 13:29:49 +0100

    Lexicon support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const vegito = require('vegito');
 const fs = require('fs');
 const intentUtteranceExpander = require('intent-utterance-expander');
+const config = require('./config.json');
 
-function parseFile(filename) {
+function parseFile(filename, lexicon = null) {
     return new Promise((resolve, reject) => {
       fs.readFile(filename, 'utf8', (err, fileContents) => {
           err ? reject(err) : resolve(fileContents);
@@ -9,11 +11,24 @@ function parseFile(filename) {
     }).then( (fileContents) => {
         let utterances = [];
         fileContents.split('\n').filter(ln => (ln != '')).map( (ln) => { // Iterate file line by line if not empty line
-            let verbose = intentUtteranceExpander(ln);
+            let lexFilter = vegito(ln, lexToList(lexicon), config);
+            let verbose = intentUtteranceExpander(lexFilter);
             utterances.push(...verbose);
         });
         return utterances;
     });
+}
+
+function lexToList(lexicon)
+{
+  let data = {};
+  if (lexicon != null) {
+    Object.entries(lexicon).map(entry => {
+      let [key,phrases] = entry;
+      data[key] = '(' + phrases.join('|') + ')';
+    });
+  }
+  return data;
 }
 
 module.exports = parseFile;
--- test/index.js
+++ test/index.js
@@ -1,14 +1,34 @@
 const signale = require('signale');
 const verboseUtterance = require('./../src/index');
+const greetingLex = require('./../lexicon/greeting.json');
+const newsLex = require('./../lexicon/news.json');
+const weatherLex = require('./../lexicon/weather.json');
 
 let filenames = [
+    // Samples without lexicon
     './samples/greeting.utter',
     './samples/news.utter',
-    './samples/weather.utter'
+    './samples/weather.utter',
+    // Sample using lexicon
+    './samples/greeting.lex.utter',
+    './samples/news.lex.utter',
+    './samples/weather.lex.utter',
 ];
 
 filenames.forEach( function(filename) {
-    verboseUtterance(filename).then( (utterances) => {
+    let lex = null;
+    switch (filename) {
+      case './samples/greeting.lex.utter':
+        lex = greetingLex;
+      break;
+      case './samples/news.lex.utter':
+        lex = newsLex;
+      break;
+      case './samples/weather.lex.utter':
+        lex = weatherLex;
+      break;
+    }
+    verboseUtterance(filename, lex).then( (utterances) => {
         signale.success(`Filename (${filename}) results:`);
         console.log(utterances);
     });
